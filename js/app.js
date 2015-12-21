b858d4b36b3dd8beb5c97b3771adaf911477626c
Enterthusiast <bernard.damien@gmail.com>
2015-12-21 23:41:03 +0100

    Fix scrolling Ad

---
+++ js/app.js
@@ -0,0 +1,50 @@
+"use strict";
+
+$(document).ready(function () {
+
+	var scrollingAd = function() {
+
+		var $starwarsAd = $('.article-one-ad-img');
+
+		var left = $starwarsAd.position().left.toString() + "px";
+		var width = $starwarsAd.width().toString() + "px";
+		var height = $starwarsAd.height().toString() + "px";
+		var top = 1125 - $starwarsAd.height();
+		top = top.toString() + "px";
+		var scroll;
+
+		$(window).scroll(function() {
+
+			scroll = $(window).scrollTop();
+
+			if(scroll > 525 && scroll < 1125 && $starwarsAd.css("top") !== "75px") {
+				$starwarsAd.css({
+					"position": "fixed",
+					"top": "75px",
+					"left": left,
+					"width": width,
+					"height": height,
+				});
+			} else if(scroll < 525 && $starwarsAd.css("top") !== "0px") {
+				$starwarsAd.css({
+					"position": "static",
+					"top": "0px"
+				});
+			} else if(scroll >= 1125 && $starwarsAd.css("position") !== "relative") {
+				$starwarsAd.css({
+					"position": "relative",
+					"top": top,
+					"left": 0,
+					"width": width,
+					"height": height,
+					"z-index": "-1"
+				});
+			}
+
+		});
+
+	}
+
+	scrollingAd();
+
+})
